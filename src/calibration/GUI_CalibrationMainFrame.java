df9d9e45fe0066605d101e82331499b4aba07477
Bruce Thomson <bwt3@hw.ac.uk>
2015-10-26 18:17:37 +0000

    Linear Pot Calibration Module Started

--- src/calibration/GUI_CalibrationMainFrame.java
+++ src/calibration/GUI_CalibrationMainFrame.java
@@ -1,5 +1,33 @@
 package calibration;
 
-public class GUI_CalibrationMainFrame {
+//Importing Libraries
+import javax.swing.*;
 
+import mainframe.GUI_MainFrame;
+
+import java.awt.*;
+import startprogram.StartProgram;
+
+/**Class to Show A Calibration Options for Calibrating Sensors*/
+public class GUI_CalibrationMainFrame extends JFrame {
+
+	//Variables to hold the startProgram and the mainFrame
+	StartProgram startProgram;
+	GUI_MainFrame main_FRA;
+	
+	public GUI_CalibrationMainFrame(StartProgram startProgram,GUI_MainFrame mainFrame)
+	{
+		this.startProgram = startProgram;
+		this.main_FRA = mainFrame;
+	}
+	
+	
+	//Method to initialise the GUI
+	private void initialiseGUI()
+	{
+		this.setTitle("Calibrate Sensors");
+		this.setSize("400,400");
+		
+	}
+	
 }
---
+++ src/calibration/GUI_LinearPot.java
@@ -0,0 +1,25 @@
+package calibration;
+
+import javax.swing.*;
+import java.awt.*;
+
+/*Class For the Linear Potentiometer's Panel*/
+public class GUI_LinearPot extends JPanel {
+
+	
+	public GUI_LinearPot() 
+	{
+		
+	}
+	
+	//Initialise GUI
+	private  void initialiseGUI()
+	{
+		this.setLayout(new GridLayout(1,2));
+		
+		JLabel initialisePot_LAB = new JLabel("Suspension Travel Sensor (Linear Potentiometer)");
+		JButton initialisePot_BUT = new JButton("Initialise");
+		this.add(initialisePot_LAB);
+		this.add(initialisePot_BUT);
+	}
+}
